15f743323d2c5f26d5eca7212149aab7438c6a3c
Oleksandr Hubanov <alexgubanow@ukr.net>
2019-06-19 15:20:43 +0300

    changed name convention, added _t to end of class name

--- trafficLightTask/lght.cpp
+++ trafficLightTask/lght.cpp
@@ -1,45 +1,60 @@
 #include "lght.h"
 #include <stdio.h>
+#include <chrono>
+#include <thread>
+using namespace std;
 
-lght::lght(lghtColor initColor, float _delay, int _priority, int _idx)
+lght_t::lght_t(lghtColor initColor, float _delay, int _priority, int _idx, int * _TopIdx)
 {
 	isCanRun = 1;
+	TopIdx = _TopIdx;
 	idx = _idx;
 	currLight = initColor;
 	delay = _delay;
 	priority = _priority;
 }
 
-int lght::wLoop()
+int lght_t::wLoop()
 {
 	while (isCanRun)
 	{
+		//pushing request (idx+priority) to queue
+		pushRequest();
+		//can be replaced by listening some port or other external interface
+		while (*TopIdx != idx) { }
 		//sw to green
-		//wait for allowed delay
+		swLight(lghtColor::Grn);
+		//wait for setted delay
+		std::this_thread::sleep_for(std::chrono::milliseconds(delay));
 		//sw to red
+		swLight(lghtColor::Grn);
+		//wait for setted delay
+		std::this_thread::sleep_for(std::chrono::milliseconds(delay));
 	}
 	return closeGate();
 }
 
-int lght::closeGate()
+int lght_t::closeGate()
 {
 	swLight(lghtColor::Red);
 	return isCanRun = 0;
 }
 
-int lght::isCanSw()
+int lght_t::swLight(lghtColor target)
+{
+	//sw to yel
+	clearQueuePlace();
+	//recognize direction of transition
+	//sw to target
+	return 0;
+}
+
+int lght_t::pushRequest()
 {
 	return 0;
 }
 
-int lght::swLight(lghtColor)
-{
-	//recognize direction of transition
-	//sw to yel
-	//sw to target
-	return 0;
-}
-int lght::swLight()
+int lght_t::clearQueuePlace()
 {
 	return 0;
 }
--- trafficLightTask/lght.h
+++ trafficLightTask/lght.h
@@ -2,19 +2,20 @@
 //enum to describe possible colors of each traffic light
 enum lghtColor { Red, Ylw, Grn };
 
-class lght
+class lght_t
 {
 private:
 	lghtColor currLight;
-	float delay;
+	int delay;
+	int* TopIdx;
 	int priority;
-	int isCanSw();
-	int swLight(lghtColor);
-	int swLight();
+	int swLight(lghtColor target);
+	int pushRequest();
+	int clearQueuePlace();
 	int volatile isCanRun;
 public:
 	int idx;
-	lght(lghtColor initColor, float _delay, int _priority, int _idx);
+	lght_t(lghtColor initColor, float _delay, int _priority, int _idx, int* _TopIdx);
 	int wLoop();
 	int closeGate();
 };
