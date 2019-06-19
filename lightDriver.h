c90d65c2f79fe1457bc4a76667b1d5cab8937c56
Oleksandr Hubanov <alexgubanow@ukr.net>
2019-06-19 23:59:35 +0300

    just put fake return to suppress warning

--- lightDriver.h
+++ lightDriver.h
@@ -2,4 +2,4 @@
 //enum to describe possible colors of each traffic light
 enum lghtColor { Red, Ylw, Grn };
 
-lghtColor inline turnTo(lghtColor clr) { /*some hardware func*/ };
+lghtColor inline turnTo(lghtColor clr) { /*some hardware func*/return clr;};
