078180211237f63a68a508f06d20d3187fade1b4
Oleksandr Hubanov <alexgubanow@ukr.net>
2019-06-19 16:00:41 +0300

    possible alpha ver of traffic light class

--- trafficLightTask/lght.cpp
+++ trafficLightTask/lght.cpp
@@ -4,57 +4,68 @@
 #include <thread>
 using namespace std;
 
-lght_t::lght_t(lghtColor initColor, float _delay, int _priority, int _idx, int * _TopIdx)
+lght_t::lght_t(lghtColor initColor, float _delay, int _priority, int _idx, router_t* _routerInst)
 {
 	isCanRun = 1;
-	TopIdx = _TopIdx;
+	routerInst = _routerInst;
 	idx = _idx;
 	currLight = initColor;
 	delay = _delay;
 	priority = _priority;
 }
-
 int lght_t::wLoop()
 {
+	rqForG request;
 	while (isCanRun)
 	{
+		//update info for request
+		request.idx = idx;
+		request.priority = priority;
 		//pushing request (idx+priority) to queue
-		pushRequest();
+		routerInst->pushRequest(request);
 		//can be replaced by listening some port or other external interface
-		while (*TopIdx != idx) { }
+		while (routerInst->getCurrIdx() != idx) { }
 		//sw to green
 		swLight(lghtColor::Grn);
 		//wait for setted delay
 		std::this_thread::sleep_for(std::chrono::milliseconds(delay));
 		//sw to red
-		swLight(lghtColor::Grn);
+		swLight(lghtColor::Red);
 		//wait for setted delay
 		std::this_thread::sleep_for(std::chrono::milliseconds(delay));
 	}
 	return closeGate();
 }
-
 int lght_t::closeGate()
 {
+	//going to red
 	swLight(lghtColor::Red);
+	//prevent to run main loop
 	return isCanRun = 0;
 }
 
 int lght_t::swLight(lghtColor target)
 {
-	//sw to yel
-	clearQueuePlace();
-	//recognize direction of transition
-	//sw to target
+	//remove self from Queue
+	routerInst->clearQueuePlace(idx);
+	//turn on yellow
+	currLight = turnTo(lghtColor::Ylw);
+	//wait for pretty transition
+	std::this_thread::sleep_for(std::chrono::milliseconds(getSmallDelay()));
+	//turn on target color
+	currLight = turnTo(target);
 	return 0;
 }
-
-int lght_t::pushRequest()
+int lght_t::getSmallDelay()
 {
-	return 0;
-}
-
-int lght_t::clearQueuePlace()
-{
-	return 0;
+	//calc small delay as 1/4 from setted delay
+	int smallDelay = delay / 4;
+	//in case if setted delay less than 4, small delay equal to 1
+	if (smallDelay < 1)
+	{
+		smallDelay = 1;
+	}
+	//or maybe it has to be zero
+	//smallDelay = 0;
+	return smallDelay;
 }
--- trafficLightTask/lght.h
+++ trafficLightTask/lght.h
@@ -1,21 +1,35 @@
 #pragma once
-//enum to describe possible colors of each traffic light
-enum lghtColor { Red, Ylw, Grn };
+#include "router.h"
+#include "lightDriver.h"
 
 class lght_t
 {
 private:
+	/*pointer to instanse of router*/
+	router_t* routerInst;
+	/*current color of light*/
 	lghtColor currLight;
+	/*setted delay*/
 	int delay;
-	int* TopIdx;
+	/*setted priority*/
 	int priority;
+	/*Method to switch color from current to target, transition going with yellow in middle
+	* args:
+	* target - desired color
+	*/
 	int swLight(lghtColor target);
-	int pushRequest();
-	int clearQueuePlace();
+	/*Method to get some small delay for yellow color on state*/
+	int getSmallDelay();
+	/*flag of working posibility*/
 	int volatile isCanRun;
 public:
+	/*index of this*/
 	int idx;
-	lght_t(lghtColor initColor, float _delay, int _priority, int _idx, int* _TopIdx);
+	lght_t(lghtColor initColor, float _delay, int _priority, int _idx, router_t* _routerInst);
+	/*Method with main loop, can be stoped by switching isCanRun to 0*/
 	int wLoop();
+	/*Method to stop running main loop
+	* draft version
+	*/
 	int closeGate();
 };
