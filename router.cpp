ea199589488c0b080bf74740edf16f66d31bff9b
Oleksandr Hubanov <alexgubanow@ukr.net>
2019-06-21 14:14:34 +0300

    actually queue don't need to be threadsafe anymore, because its arranged before running parallel

--- router.cpp
+++ router.cpp
@@ -1,45 +1,53 @@
 #include "router.h"
 #include <thread>
-router_t::router_t()
-{
-}
 
+/*return iterator to active member in queue*/
 std::map<int, int>::iterator router_t::getTopIdx()
 {
 	return TopIdx->getItr();
 }
 
+/*return iterator to first member in queue*/
 std::map<int, int>::iterator router_t::getFqe()
 {
-	return queue->begin();
+	return queue.begin();
 }
-
+/*return iterator to last member in queue*/
 std::map<int, int>::iterator router_t::getLqe()
 {
-	return std::prev(queue->end());
+	//actually returned iterator is prelast -> plus one dirty fix, need to get rid of it
+	return std::prev(queue.end());
 }
-
+/*Setup active iterator in queue*/
 void router_t::setTopIdx(std::map<int, int>::iterator newTop)
 {
+	//set active iterator in queue to given one
 	TopIdx->setItr(newTop);
 }
 
+/*Moving active iterator in queue*/
 int router_t::nextPls()
 {
+	//dirty fix to avoid collisions, after while can happen that two lght will go green
+	//probably it comes from developed principle of switching work
 	std::this_thread::sleep_for(std::chrono::nanoseconds(100));
+	//check if currently last member in queue is active
 	if (TopIdx->getItr() != getLqe())
 	{
+		//increment iterator of active member
 		TopIdx->incItr();
 	}
 	else
 	{
-		TopIdx->setItr(queue->begin());
+		//set iterator of active member to first
+		TopIdx->setItr(queue.begin());
 	}
 	//in future can return time to wait until
 	return 0;
 }
-
+/*Pushing given iterator into queue*/
 std::map<int, int>::iterator router_t::pushRequest(std::pair<int, int> rq)
 {
-	return queue->insert(queue->lower_bound(rq.first), rq);
+	//finding best place as first element equal to or greater than given priority, or end()
+	return queue.insert(queue.lower_bound(rq.first), rq);
 }
--- router.h
+++ router.h
@@ -1,21 +1,27 @@
 #pragma once
 #include <map>
-#include <mutex>
-#include <atomic>
 #include "safePtr.h"
 #include "safeItr.h"
 
+/*Router to manage swithing quene of traffic lights*/
 class router_t
 {
 private:
+	/*iterator of active member in queue*/
 	safe_ptr<SafeItr> TopIdx;
-	safe_ptr<std::map<int, int>> queue;
+	/*sorted queue, first is priority, second is idx*/
+	std::map<int, int> queue;
 public:
-	router_t();
+	/*return iterator to active member in queue*/
 	std::map<int, int>::iterator getTopIdx();
+	/*return iterator to first member in queue*/
 	std::map<int, int>::iterator getFqe();
+	/*return iterator to last member in queue*/
 	std::map<int, int>::iterator getLqe();
+	/*Setup active iterator in queue*/
 	void setTopIdx(std::map<int, int>::iterator newTop);
+	/*Moving active iterator in queue*/
 	int nextPls();
+	/*Pushing given iterator into queue*/
 	std::map<int, int>::iterator pushRequest(std::pair<int, int> rq);
 };
